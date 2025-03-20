35e70b988bee9525f648a602941483deeb26be83
Emanuelly Lopes Miranda <lopesmanu311208@gmail.com>
2025-03-20 14:23:49 -0400

    Add files via upload

---
+++ Ex3.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Insira o primeiro número"))
+numero2 = int(input("Insira o segundo número"))
+soma = numero1 + numero2
+sub = numero1 - numero2
+mult = numero1 * numero2
+print(f"A soma desses números foi {soma}")
+print(f"A subtração desses números foi {sub}")
+print(f"A multiplicação desses números foi {mult}")
